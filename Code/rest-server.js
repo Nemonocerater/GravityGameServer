8075efe215118ef5fbb077a159a961c6056e78bb
Joshua Harris <joshua40harris@gmail.com>
2014-12-19 18:15:35 -0500

    not sure... been months

--- Code/rest-server.js
+++ Code/rest-server.js
@@ -10,6 +10,15 @@ module.exports.getServer = function getServer(port, leaderboard_connector) {
 	
 	app.use(bodyParser.urlencoded({ extended: true }));
 	app.use(bodyParser.json());
+	app.use(function(req, res, next) {
+		console.log('%s %s', req.method, req.url);
+		next();
+	});
+	app.all('/', function(req, res, next) {
+		res.header("Access-Control-Allow-Origin", "*");
+		res.header("Access-Control-Allow-Headers", "X-Requested-With");
+		next();
+	});
 	leaderboard.decorate (app, leaderboard_connector);
 	
 	var server = http.createServer(app);
--- setup.sh
+++ setup.sh
@@ -11,17 +11,24 @@ rpm -Uvh http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noar
 
 rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
 
+
+###################
+# Redis
 yum install redis -y
 
 # Starts redis and then returns control to the shell
 redis-server &
 
-
+##################
 # Node
 yum install wget -y
 wget http://nodejs.org/dist/v0.10.30/node-v0.10.30-linux-x64.tar.gz
 sudo tar --strip-components 1 -xzvf node-v* -C /usr/local
 
+
+###################
+# Goodies
+yum install screen -y
 
 ###########################
 # Just for Josh
